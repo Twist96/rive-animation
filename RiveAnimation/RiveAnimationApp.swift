60aeee3ea4c0922a4124729cd3a37c637b043871
Matthew <matthew1@enigmalabs.io>
2023-02-16 18:49:05 +0100

    Initial Commit

---
+++ RiveAnimation/RiveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimationApp.swift
+//  RiveAnimation
+//
+//  Created by Matthew Chukwuemeka on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
